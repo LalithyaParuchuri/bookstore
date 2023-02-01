e5de48bb4fdbbb0d71d06c2f923ca71e8f37b6a8
Ctl-user <ctl-user@Ctl-users-MacBook-Pro.local>
2023-02-01 16:14:37 -0500

    Add style changes

--- src/components/book/index.jsx
+++ src/components/book/index.jsx
@@ -34,10 +34,18 @@ export const Book = ({ id, title, author, price, description, imageUrl }) => {
       <img className="cart" src={cart} onClick={addBookToCartHanlder} />
       <img className="img" src={imageUrl} />
       <div className="info">
-        <p>Book Title: {title}</p>
-        <p>Author: {author}</p>
-        <p>Price: {price}</p>
-        <p>Description: {description}</p>
+        <p>
+          <b>Book Title:</b> {title}
+        </p>
+        <p>
+          <b>Author:</b> {author}
+        </p>
+        <p>
+          <b>Price:</b> {price}
+        </p>
+        <p>
+          <b>Description:</b> {description}
+        </p>
       </div>
     </div>
   );
