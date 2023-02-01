cc719961ba70c62e90d8a508236ed0878c628aea
Ctl-user <ctl-user@Ctl-users-MacBook-Pro.local>
2023-02-01 10:26:09 -0500

    Add cart icon on book

--- src/components/book/index.jsx
+++ src/components/book/index.jsx
@@ -1,9 +1,10 @@
 import "./style.css";
-
+import cart from "../../assets/cart_icon.png";
 export const Book = ({ title, author, price, description, imageUrl }) => {
   return (
     <div className="book-container">
-      <img src={imageUrl} />
+      <img className="cart" src={cart} />
+      <img className="img" src={imageUrl} />
       <div className="info">
         <p>Book Title: {title}</p>
         <p>Author: {author}</p>
