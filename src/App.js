48eca9d06cc2fbe90bc1dcb9d32b3612ac97d3a1
Ctl-user <ctl-user@Ctl-users-MacBook-Pro.local>
2023-02-01 08:42:58 -0500

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { RouterProvider } from "react-router-dom";
+import { Header } from "./components/header";
 import { router } from "./router";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <RouterProvider router={router} />
     </div>
   );
